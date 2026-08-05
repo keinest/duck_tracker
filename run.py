057703c2487456349758a9d6dc528e36537618d3
soulkingOverkill <linedevils271@gmail.com>
2026-08-05 09:59:14 +0100

    Update run.py

--- run.py
+++ run.py
@@ -1,7 +1,9 @@
-# duc_tracker/run.py
+import os
 from app import create_app
+from app.config import DevelopmentConfig, ProductionConfig
 
-app = create_app()
+config_class = ProductionConfig if os.environ.get('FLASK_ENV') == 'production' else DevelopmentConfig
+app = create_app(config_class)
 
 if __name__ == '__main__':
     app.run(debug=True)
